39a23a03864f9bd88f9463d0a9c93c6db32dc19a
Kiiro <ajxx030217@163.com>
2025-09-17 16:27:44 -0400

    feat: 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,30 +1,26 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, ScrollView } from 'react-native'
 import Constants from 'expo-constants'
 import theme from '../theme'
 import AppBarTab from './AppBarTab'
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.textPrimary,
     height: 100
   },
-  text: {
-    color: theme.colors.textWhite,
-    fontSize: theme.fontSizes.subheading,
-    fontWeight: theme.fontWeights.bold,
-    padding: theme.fontSizes.subheading
+  tab: {
+    alignItems: 'center'
   }
 })
 
-const AppBar = () => {
-  return <View style={styles.container}>
-    <AppBarTab item='Repositories' to='/' />
-    <AppBarTab item='Sign in' to='/signin' />
+const AppBar = () => (
+  <View style={styles.container}>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.tab}>
+      <AppBarTab item='Repositories' to='/' />
+      <AppBarTab item='Sign in' to='/signin' />
+    </ScrollView>
   </View>
-}
+)
 
 export default AppBar
